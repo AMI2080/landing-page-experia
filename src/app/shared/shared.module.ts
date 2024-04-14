de11da3684352101dfe02ce23149ae0f8b76717e
AMI2080 <AMI_2080@Yahoo.com>
2024-04-14 11:14:46 +0200

    feat(app): translate title

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,13 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Renderer2, RendererFactory2 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PageComponent } from './components/page/page.component';
 import { HeaderComponent } from './components/page/header/header.component';
 import { FooterComponent } from './components/page/footer/footer.component';
 import { HttpClient, provideHttpClient, withFetch } from '@angular/common/http';
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateModule, TranslateLoader, TranslateService, LangChangeEvent } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { RouterModule } from '@angular/router';
+import { Title } from '@angular/platform-browser';
 
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http);
@@ -19,8 +20,6 @@ const sharedComponents = [PageComponent, HeaderComponent, FooterComponent];
     CommonModule,
     RouterModule,
     TranslateModule.forRoot({
-      defaultLanguage: 'ar',
-      useDefaultLang: true,
       loader: {
         provide: TranslateLoader,
         useFactory: HttpLoaderFactory,
@@ -39,4 +38,34 @@ const sharedComponents = [PageComponent, HeaderComponent, FooterComponent];
   ],
   providers: [provideHttpClient(withFetch())],
 })
-export class SharedModule {}
+export class SharedModule {
+  private renderer: Renderer2;
+
+  public constructor(
+    rendererFactory: RendererFactory2,
+    translate: TranslateService,
+    titleService: Title
+  ) {
+    this.renderer = rendererFactory.createRenderer(null, null);
+    if (
+      typeof document !== 'undefined' &&
+      typeof localStorage !== 'undefined'
+    ) {
+      const lang = localStorage.getItem('lang') ?? 'ar';
+      translate.setDefaultLang('ar');
+      translate.use(lang);
+
+      translate.onLangChange.subscribe((event: LangChangeEvent) => {
+        const dir = event.lang === 'ar' ? 'rtl' : 'ltr';
+        localStorage.setItem('lang', event.lang);
+        this.renderer.setAttribute(
+          document.documentElement,
+          'lang',
+          event.lang
+        );
+        this.renderer.setAttribute(document.documentElement, 'dir', dir);
+        titleService.setTitle(translate.instant('translate_title'));
+      });
+    }
+  }
+}
